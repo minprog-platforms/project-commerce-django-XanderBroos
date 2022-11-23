b30b40531cf7b310d0fed2d7d72fec20ce116935
XanderBroos <broosxander@gmail.com>
2022-11-23 20:16:30 +0100

    admin control

--- auctions/models.py
+++ auctions/models.py
@@ -7,6 +7,18 @@ class User(AbstractUser):
 
 
 class Listing(models.Model):
+    """
+    Vereiste items voor een listing:
+    title van de listing
+    omschrijving van listing
+    koopprijs van listing 
+    minimum prijs om te bieden
+    Moet kijken of listing nog active is 
+    owner van de listing, verkoper dus
+    Huidig hoogste bid van user
+
+    Moet ook nog verbindingen gaan maken met de classes comments en bids
+    """
     # items of listings
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=400)
@@ -16,8 +28,8 @@ class Listing(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user")
     image = models.CharField(max_length=1000)
     current_bid = models.IntegerField()
-    bidder = models.CharField(max_length=1000)
-    
+    current_bidder = models.CharField(max_length=1000, default="Xander")
+   
 
     def __str__(self) -> str:
         return f"{self.title}"
@@ -25,18 +37,39 @@ class Listing(models.Model):
 
 
 class Bid(models.Model):
-    # user =  models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user")
-    
-    # title_item = models.ForeignKey(Listing, on_delete=models.CASCADE, blank=True, related_name="title")
-    # buyer =  models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user")
-    # bid = models.IntegerField(min_length="min_bid", max_length="price") 
-    # current_bid = models.IntegerField()
-    # def __str__(self) -> str:
-    #     return f"{self.title}"
+    """
+    Wordt alleen gebruikt in listing page.
+    Heeft de items, zodat een user een bid kan plaatsen, en dat de class dan ook de gegevens heeft
+    over wie heeft geboden, de user, en op welk item is geboden. 
+    bid_user is de user die nu is ingelogd, en die blijft hetzelfde, ook als iemand anders is ingelogd
+    title_item zorgt dat het bod op item, hetzelfde item is als de item van de huidige pagina
+
+    """
+    # bid_user =  models.ForeignKey(User, on_delete=models.CASCADE, blank=True,  related_name="bid_user")
+    title_item = models.CharField(max_length=1000)
+    bid = models.IntegerField() 
+    bid_user = models.CharField(max_length=1000)
+
+    def __str__(self) -> str:
+        return f"{self.bid_user}"
     pass
 
 
 class Comment(models.Model):
-    # commenter =models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user")
-    # comment =  models.CharField(max_length=1000)
+    """
+    Wordt alleen gebruikt wanneer je op de listing pagina bent.
+    Heeft de items, zodat je een comment kan opslaan,  en van die comment weet wie
+    die comment heeft geschreven, en bij welke item deze comment hoort
+    comment_user is de user die is ingelogd tijdens het maken van de comment
+    comment_title zorgt dat de comment alleen er staat bij de item van huidige pagina
+    comment_tekst onthoudt alle tekst in comment
+    """
+    comment_user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="commenter")
+    comment_tekst =  models.CharField(max_length=1000)
+    comment_title = models.CharField(max_length=1000)
+    
+    def __str__(self) -> str:
+        return f"{self.comment_tekst}"
+
+
     pass
--- auctions/views.py
+++ auctions/views.py
@@ -4,13 +4,14 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User, Listing
+from .models import User, Listing, Bid , Comment
 
 
 def index(request):
     entries = Listing.objects.filter(active=True)
+    closed_entries = Listing.objects.filter(active=False)
     return render(request, "auctions/index.html", {
-        "list": entries
+        "list": entries, "other_list":closed_entries
     })
 
 def listing(request, name):
@@ -18,24 +19,106 @@ def listing(request, name):
         item = Listing.objects.get(title=f"{name}")
     except Listing.DoesNotExist:
         item = None
+
+    try:
+        comment_list = Comment.objects.filter(comment_title=f"{name}")
+    except Comment.DoesNotExist:
+        comment_list = None
+
+
+
+        
     if request.method == "GET":
+
+
+        # if listing is gesloten en ingelogde user is user laatste bod
+
+        accept=False
+        if request.user == item.owner:
+            accept = True
+        con=True
+        gewonnen="ge"
+        if item.active:
+            con=False
+        else:
+            haha = str(request.user)
+            if haha == item.current_bidder:
+                gewonnen="Gefeliciteerd, je hebt gewonnen"
+            else:
+                gewonnen="Helaas, je hebt dit item niet gewonnen"
+
+        
         return render(request, "auctions/listing.html", {
-            "item": item
+            "item": item, "accept":accept, "comment":comment_list, "con":con, "test":gewonnen
         })
     else:
+        # Zorg dat je een watchlist kan toevoegen 
+
+        # check = False
+        # watch = request.POST["watch"]
+        # if watch == True:
+        #     check = True
+        
+        # verkrijg juist bid item
         # bid_item = Listing.objects.get(title=f"{name}")
-        bid = int(request.POST["bid"])
-        # user = request.user
+        try:
+            bid = int(request.POST["bid"])
+        except:
+            bid = 0
+
+        try:
+            actief = request.POST["done"]
+        except:
+            actief = "Niet"
+
+        try:
+            comment = str(request.POST["comment"])
+        except:
+            comment = "None"
+
+        
+
+        if actief == "done":
+            actief =False
+        else:
+            actief=True
+
+        # !!!! moet uit Listing, moet in Bid class
         if bid > item.current_bid:
+            # save new bid as current bid
             item.current_bid = bid
-            item.save()
+            # save current user
+            name_bid = str(request.user)
+            item.current_bidder = name_bid
+            
+
+            
+            # save the whole item
+            
+
+        item.active=actief
+        item.save()
+
+        if comment != "None":
+            comment_for = Comment(
+                comment_user=request.user,
+                comment_title=f"{name}",
+                comment_tekst="placeholder"
+            )
+            comment_for.comment_tekst=comment
+            comment_for.save()
+            
+
+
+        # Zorg dat je comment kan plaatsen
+            # gebruik post comment
+            # update lijst van alle comments in Comments class
+
+        # Zorg dat je auction kan sluiten met button
+            # als gesloten, scherm moet zeggen dat gewonnen
+        
+        # Redirect response and go back to homepage
         return HttpResponseRedirect(reverse(index))
-        return render(request, "auctions/listing.html", {
-            "item": item
-        })
-
-    pass
-
 
 def create(request):
     if request.method == "POST":
@@ -52,6 +135,7 @@ def create(request):
             price=bid,
             min_bid=min_bid,
             active=True,
+            current_bid=min_bid,
             owner=user,
             image=url)
 
